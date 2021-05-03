f12dae20e825cc69332e11d51b3fbdfc75475baa
webcornerpoint <77179144+webcornerpoint@users.noreply.github.com>
2021-05-03 10:03:08 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}} updated from repositroy</div>`,
 })
 export class AppComponent {
   value = 'World';
